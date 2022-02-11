a860619a029ac220313e0356b654a0ba53c39095
Walter Okpala <cwokpala@uwaterloo.ca>
2022-02-11 18:09:49 -0500

    This is a hashMap Implementation of Two_Sum

---
+++ Arrays/Two_sum.js
@@ -0,0 +1,25 @@
+/* @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    if (nums == []) {
+        return
+    }
+
+    let hashMap = {};
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let val = (target - nums[left])
+        if (val in hashMap) {
+            return [left, hashMap[val]]
+        } else {
+            hashMap[nums[left]] = left;
+            left += 1
+        }
+    }
+
+
+}
+
+console.log(twoSum([2, 7, 11, 15], target = 9))
