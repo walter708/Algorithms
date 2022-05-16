83ae8f831bf9b0f4551ac7dafc2757f9ef190c0f
walterokpala <55052227+walter708@users.noreply.github.com>
2022-05-16 16:35:59 -0400

    Time: 83 ms (88.74%), Space: 14.7 MB (17.54%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.py
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -1,15 +1,27 @@
 class Solution:
-    def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        i , j = 0 , len(nums) - 1
+#     def sortArrayByParity(self, nums: List[int]) -> List[int]:
+#         i , j = 0 , len(nums) - 1
         
-        while i < j:
-            if nums[i] % 2 > nums[j] % 2:
-                nums[i] , nums[j] = nums[j] , nums[i]
+#         while i < j:
+#             if nums[i] % 2 > nums[j] % 2:
+#                 nums[i] , nums[j] = nums[j] , nums[i]
             
-            if nums[i] % 2 == 0: i+=1
-            if nums[j] % 2 == 1: j-=1
-        return nums
-         
+#             if nums[i] % 2 == 0: i+=1
+#             if nums[j] % 2 == 1: j-=1
+#         return nums
+    # Time complexity O(n)
+    # Space complexity O(1)
+    
+    # OR
+    # def sortArrayByParity(self, nums: List[int]) -> List[int]:
+    #     return ([i for i in nums if i % 2 == 0 ] + [j for j in nums if j % 2 == 1 ])
+    # Time complexity O(n)
+    # Space complexity O(1)   
+    
+    # OR
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        nums.sort(key = lambda x : x % 2)
+        return nums 
 
                 
         
