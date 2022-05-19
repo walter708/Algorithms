2cdf8662091c3edc327b203ed73a06cacf382225
walterokpala <55052227+walter708@users.noreply.github.com>
2022-05-18 23:25:40 -0400

    Time: 98 ms (42.28%), Space: 13.9 MB (44.66%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -0,0 +1,69 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        remainder = 0 
+        dummy = ListNode()
+        tail = dummy 
+        while l1 and l2:
+            res = l1.val + l2.val  #  9 + 9 = 18 
+            res = res + remainder  # 18 + 0 = 18 
+            remainder = 0      
+            if res >= 10:  # 18 >  #  
+                l1.val = res % 10  # 8 
+                tail.next = l1     #  8 
+                remainder = res // 10  # 1
+            else:    
+                l1.val = res
+                tail.next = l1
+            l1 = l1.next
+            l2 = l2.next
+            tail = tail.next
+        while l1:
+            res = l1.val + remainder 
+            remainder = 0 
+            print(res)
+            if res >= 10:
+                l1.val = res % 10
+                tail.next = l1
+                remainder = res // 10
+            else:
+                l1.val = res
+                tail.next = l1
+            tail = tail.next
+            l1 = l1.next
+        while l2:
+            res = l2.val + remainder # 9 + 
+            remainder = 0 
+            if res >= 10:
+                l2.val = res % 10
+                tail.next = l2
+                remainder = res // 10
+            else:
+                l2.val = res
+                tail.next = l2
+            tail = tail.next
+            l2 = l2.next
+        if remainder:
+            lastNode = ListNode(remainder)
+            tail.next = lastNode
+        return dummy.next
+    # 2 4
+            
+                
+            
+        
+        
+        # 2 -> 4 -> 3 ==> 342
+        # 5 -> 6 -> 4 ==> 465
+        # 7    10 % 10 = 0 10// 10 = 1 
+        # remainder = 1 
+        #           7 
+        
+        # 2 -> 4 -> 3 -> 6 list 
+        # 5 -> 6 -> 4
+        
+        
