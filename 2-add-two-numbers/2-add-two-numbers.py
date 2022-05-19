5bd250a99863ccee6011b988923f2545135eca5a
walterokpala <55052227+walter708@users.noreply.github.com>
2022-05-19 16:19:01 -0400

    Time: 84 ms (59.52%), Space: 14.1 MB (10.74%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,75 +5,25 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        remainder = 0 
-        dummy = ListNode()
-        tail = dummy 
-        while l1 and l2:
-            res = l1.val + l2.val  #  9 + 9 = 18 
-            res = res + remainder  # 18 + 0 = 18 
-            remainder = 0      
-            # if res >= 10:  # 18 >  #  
-            #     l1.val = res % 10  # 8 
-            #     tail.next = l1     #  8 
-            #     remainder = res // 10  # 1
-            # else:    
-            #     l1.val = res
-            #     tail.next = l1
-            remainder = self.refactor(res, l1 , tail, remainder)
-            l1 = l1.next
-            l2 = l2.next
-            tail = tail.next
-        while l1:
-            res = l1.val + remainder 
-            remainder = 0 
-            print(res)
-            # if res >= 10:
-            #     l1.val = res % 10
-            #     tail.next = l1
-            #     remainder = res // 10
-            # else:
-            #     l1.val = res
-            #     tail.next = l1
-            remainder =self.refactor(res, l1 , tail, remainder)
-            tail = tail.next
-            l1 = l1.next
-        while l2:
-            res = l2.val + remainder # 9 + 
-            remainder = 0 
-            # if res >= 10:
-            #     l2.val = res % 10
-            #     tail.next = l2
-            #     remainder = res // 10
-            # else:
-            #     l2.val = res
-            #     tail.next = l2
-            remainder =self.refactor(res, l2 , tail, remainder)
-            tail = tail.next
-            l2 = l2.next
-        if remainder:
-            lastNode = ListNode(remainder)
-            tail.next = lastNode
-        return dummy.next
-    # 2 4
+        def add(head1 , head2 , carry = 0):
+            if not head1 and not head2 and not carry:
+                return None 
+            val1 = 0 if head1 == None else head1.val
+            val2 = 0 if head2 == None  else head2.val
+            res = val1 + val2 + carry
+            nextCarry = 1 if res > 9 else 0
+            digit = res % 10 
+            resultNode = ListNode(digit)
+            next1 = None if head1 == None else head1.next
+            next2 = None if head2 == None else head2.next
+            resultNode.next = add(next1 , next2 , nextCarry)
+            return resultNode
+        return add(l1 , l2)
             
-    def refactor(self, res, head , tail, remainder):
-        if res >= 10:  # 18 >  #  
-            head.val = res % 10  # 8 
-            tail.next = head     #  8 
-            remainder = res // 10  # 1
-        else:    
-            head.val = res
-            tail.next = head
-        return remainder
+            
+
         
         
-        # 2 -> 4 -> 3 ==> 342
-        # 5 -> 6 -> 4 ==> 465
-        # 7    10 % 10 = 0 10// 10 = 1 
-        # remainder = 1 
-        #           7 
-        
-        # 2 -> 4 -> 3 -> 6 list 
-        # 5 -> 6 -> 4
+
         
         
