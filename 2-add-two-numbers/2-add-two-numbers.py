b8b6840328784c2094573300a520283d1b82d552
walterokpala <55052227+walter708@users.noreply.github.com>
2022-05-18 23:41:12 -0400

    Time: 122 ms (19.46%), Space: 14 MB (10.74%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -12,13 +12,14 @@ class Solution:
             res = l1.val + l2.val  #  9 + 9 = 18 
             res = res + remainder  # 18 + 0 = 18 
             remainder = 0      
-            if res >= 10:  # 18 >  #  
-                l1.val = res % 10  # 8 
-                tail.next = l1     #  8 
-                remainder = res // 10  # 1
-            else:    
-                l1.val = res
-                tail.next = l1
+            # if res >= 10:  # 18 >  #  
+            #     l1.val = res % 10  # 8 
+            #     tail.next = l1     #  8 
+            #     remainder = res // 10  # 1
+            # else:    
+            #     l1.val = res
+            #     tail.next = l1
+            remainder = self.refactor(res, l1 , tail, remainder)
             l1 = l1.next
             l2 = l2.next
             tail = tail.next
@@ -26,25 +27,27 @@ class Solution:
             res = l1.val + remainder 
             remainder = 0 
             print(res)
-            if res >= 10:
-                l1.val = res % 10
-                tail.next = l1
-                remainder = res // 10
-            else:
-                l1.val = res
-                tail.next = l1
+            # if res >= 10:
+            #     l1.val = res % 10
+            #     tail.next = l1
+            #     remainder = res // 10
+            # else:
+            #     l1.val = res
+            #     tail.next = l1
+            remainder =self.refactor(res, l1 , tail, remainder)
             tail = tail.next
             l1 = l1.next
         while l2:
             res = l2.val + remainder # 9 + 
             remainder = 0 
-            if res >= 10:
-                l2.val = res % 10
-                tail.next = l2
-                remainder = res // 10
-            else:
-                l2.val = res
-                tail.next = l2
+            # if res >= 10:
+            #     l2.val = res % 10
+            #     tail.next = l2
+            #     remainder = res // 10
+            # else:
+            #     l2.val = res
+            #     tail.next = l2
+            remainder =self.refactor(res, l2 , tail, remainder)
             tail = tail.next
             l2 = l2.next
         if remainder:
@@ -53,8 +56,15 @@ class Solution:
         return dummy.next
     # 2 4
             
-                
-            
+    def refactor(self, res, head , tail, remainder):
+        if res >= 10:  # 18 >  #  
+            head.val = res % 10  # 8 
+            tail.next = head     #  8 
+            remainder = res // 10  # 1
+        else:    
+            head.val = res
+            tail.next = head
+        return remainder
         
         
         # 2 -> 4 -> 3 ==> 342
