5ec830e6fd7d605f1a6c3d778b9d6d4d40b66293
walterokpala <55052227+walter708@users.noreply.github.com>
2022-05-21 11:02:48 -0400

    Time: 775 ms (22.56%), Space: 36.5 MB (18.73%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -5,27 +5,29 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or  not head.next:
-            return head 
+        if not head or not head.next:
+            return head
+        
         left = head
-        right = self.getMiddle(head)
+        right = self.getMiddle(left)
         tmp = right.next
-        right.next = None 
+        right.next = None
         right = tmp
         left = self.sortList(left)
         right = self.sortList(right)
-        return  self.merge(left, right)
+        return self.merge(left , right)
     
-    def getMiddle(self, head:  Optional[ListNode]) -> Optional[ListNode]:
-        slow, fast = head , head.next
+    
+    def getMiddle(self , head):
+        slow , fast = head , head.next
         
         while fast and  fast.next:
             slow = slow.next
             fast = fast.next.next
         return slow
-    def merge(self, left , right)-> Optional[ListNode]:
+    
+    def merge(self, left, right):
         tail = dummy = ListNode()
-        
         while left and right:
             if left.val < right.val:
                 tail.next = left
@@ -33,12 +35,14 @@ class Solution:
             elif right.val <= left.val:
                 tail.next = right
                 right = right.next
-            tail  = tail.next
+            tail = tail.next
         if left:
             tail.next = left
         if right:
             tail.next = right
         return dummy.next
+        
+
 
             
         
