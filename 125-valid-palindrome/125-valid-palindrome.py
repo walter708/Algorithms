5a00b8428fba9fe348eee4d54a907b9f6a8f1940
walterokpala <55052227+walter708@users.noreply.github.com>
2022-05-23 10:21:19 -0400

    Time: 48 ms (86.67%), Space: 15.6 MB (16.78%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,14 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        for char in s:
-            if ord(char) < 97 or ord(char) > 122:
-                if ord(char) >= 48 and ord(char) <= 57:
-                    continue
-                s = s.replace(char,"")
-
-                
-                
+        s = re.sub("[^a-z0-9]" , "" , s)
         print(s)                
         l, r = 0 , len(s) - 1
         while l < r:
