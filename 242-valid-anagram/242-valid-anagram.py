930512bba846d0a04829026d416a3dc98fc894c5
walterokpala <55052227+walter708@users.noreply.github.com>
2022-05-26 14:16:03 -0400

    Time: 94 ms (18.05%), Space: 14.4 MB (67.28%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,23 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False 
-        countOne = {}
-        countTwo = {}
         
-        for i in range(len(s)):
-            if(s[i] not in countOne):
-                countOne[s[i]] = 0
-            countOne[s[i]] += 1
-         
-        for j in range(len(t)):
-            if(t[j] not in countTwo):
-                countTwo[t[j]] = 0
-                
-            countTwo[t[j]] += 1
-            
-        for key in countOne.keys():
-            
-            if countOne[key] != countTwo.get(key , 0):
+        countDic = {}
+        
+        for i in s:
+            if i not in countDic:
+                countDic[i] = 1
+            else:
+                countDic[i]  += 1
+        for j in t:
+            if j not in countDic:
                 return False
-        return True
+            countDic[j]-=1
+        
+        for val in countDic.values():
+            if val != 0:
+                return False
+        return True 
