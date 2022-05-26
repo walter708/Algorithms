f999de0ef02df0d74227fed80269282f6ee6665e
walterokpala <55052227+walter708@users.noreply.github.com>
2022-05-26 12:47:11 -0400

    Time: 710 ms (5.12%), Space: 28.8 MB (5.01%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS, COLS = len(grid) , len(grid[0])
+        count = 0
+        visit = set()
+        for i in range(ROWS):
+            for j in range(COLS):
+                if self.explore(grid , i , j , visit):
+                    count+=1
+        return count
+    def explore(self, grid, i , j , visit):
+        rowBound = i >= 0 and i < len(grid)
+        colBound = j >= 0 and j < len(grid[0])
+        
+        if not rowBound:return False
+        if not colBound:return False
+        pos = (i , j)
+        if pos in visit:return False
+        visit.add(pos)
+        if grid[i][j] == "0":return False
+        
+        self.explore( grid, i+1 , j , visit)
+        self.explore( grid, i-1 , j , visit)
+        self.explore( grid, i , j+1 , visit)
+        self.explore( grid, i , j-1 , visit)
+        return True 
+        
+        
+        
+        
