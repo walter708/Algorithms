1936d8bb41d9bff17c59771908a72f5df01de3b7
walterokpala <55052227+walter708@users.noreply.github.com>
2022-05-30 18:28:10 -0400

    Time: 93 ms (35.15%), Space: 14.1 MB (95.50%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,13 +1,16 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        res = [-1] * len(nums1)
+        res = []
         for i in nums1:
             index = nums2.index(i)
-            ind = nums1.index(i)
+            check = False
             for j in range((index+1) , len(nums2)):
                 if nums2[j] > i:
-                    res[ind] = nums2[j]
+                    res.append(nums2[j])
+                    check =True
                     break
+            if check == False:
+                res.append(-1)
         return res
                 
         
