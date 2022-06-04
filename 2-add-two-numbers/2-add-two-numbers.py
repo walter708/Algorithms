fd9d3a65222fee441090e502befd6f1964090c99
walterokpala <55052227+walter708@users.noreply.github.com>
2022-06-04 13:20:11 -0400

    Time: 94 ms (49.09%), Space: 13.8 MB (86.25%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,23 +5,38 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        def add(head1 , head2 , carry = 0):
-            if head1 == None and head2 == None and carry==0:
-                return None
+        dummy = ListNode()
+        tail = dummy
+        carry = 0
+        while l1 or l2 or carry != 0:
+            if l1:
+                tail.next = l1
+                tail = tail.next
+            elif l2:
+                tail.next = l2
+                tail = tail.next
+            else:
+                tail.next = ListNode()
+                tail = tail.next
+                
+            # print(dummy.next)
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
             
-            val1 = 0 if head1 == None else head1.val
-            val2 = 0 if head2 == None else head2.val
+            total = val1 + val2 + carry
+            digit = total % 10 
+            carry = total // 10
+            tail.val = digit
+
             
-            res = val1 + val2 + carry
-            nextCarry = 0 if res <=  9 else 1
-            digit = res % 10
-            next1 = None if head1 == None else head1.next
-            next2 = None if head2 == None else head2.next
-            resultNode = ListNode(digit)
-            resultNode.next = add(next1 , next2 , nextCarry)
-            return resultNode
-        return add(l1 , l2)
+        return dummy.next
+            
+            
+            
+
+
             
             
 
