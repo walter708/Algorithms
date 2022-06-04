707d2b6e76507f77ccf77208c183e7ab90eafd74
walterokpala <55052227+walter708@users.noreply.github.com>
2022-06-04 10:24:57 -0400

    Time: 1271 ms (90.27%), Space: 14.3 MB (47.19%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,39 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
-        def dfs(amount, memo):
-            if amount == 0:
-                return [] 
-            if amount < 0:
-                return -1 
-            if amount in memo:
-                return memo[amount]
-            
-            miniCoin = -1
-            
-            
-            for coin in coins:
-                rmd = amount - coin
-                
-                value = dfs(rmd, memo)
-                
-                if value != -1:
-                    comb = [i for i in value]
-                    comb.append(coin)
-                    
-                    if  miniCoin == -1 or len(comb) < len(miniCoin):
-                        miniCoin = comb
-                        
-            memo[amount] = miniCoin
-            
-            return memo[amount]
-                        
-
-                    
-
         
-        val = dfs(amount, memo)
-        return val if val == - 1 else len(val)
+        MAX = float('inf')
+        
+        dp = [0] + ([MAX] * amount)
+        
+        for  i in range(1, (amount + 1)):
+            dp[i] = min([dp[i - c] if (i - c) >=0 else MAX for c in coins]) +  1
+        
+        if dp[-1] == MAX:
+            return  - 1
+        else:
+            return dp[-1]
+
                 
             
         
