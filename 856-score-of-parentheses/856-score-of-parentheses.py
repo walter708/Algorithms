28f7ec382216dde4bc544a734681851600fd56d6
walterokpala <55052227+walter708@users.noreply.github.com>
2022-06-04 20:30:18 -0400

    Time: 69 ms (5.27%), Space: 13.9 MB (58.02%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.py
+++ 856-score-of-parentheses/856-score-of-parentheses.py
@@ -1,16 +1,18 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
+        
         stack = [0]
         
         for i in s:
+            if i == ")":
+                val = stack.pop()
+                stack[-1] += max(2 * val , 1) 
             if i == "(":
                 stack.append(0)
-            elif i == ")":
-                val = stack.pop()
-                stack[-1] += max((2 * val) , 1)
                 
         return stack[0]
             
+
         
 
         
